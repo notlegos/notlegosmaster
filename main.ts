a25bc0af4a0d87fcd12fa8dd27671862804626da
notlegos <roger@mustacich.com>
2024-09-22 11:08:46 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+function pushPrint (lineNo: number, textLine: string) {
+    if (lineNo == 2) {
+        if (textLine != pushPrint2) {
+            pushPrint2 = textLine
+            Connected.showUserText(lineNo, textLine)
+        }
+    } else {
+    	
+    }
+}
 function isNearly (reference: number, reading: number, tolerance: number) {
     if (reading >= reference - tolerance && reading <= reference + tolerance) {
         return true
@@ -5,25 +15,39 @@ function isNearly (reference: number, reading: number, tolerance: number) {
         return false
     }
 }
+function padLimit (numberIn: number, digits: number) {
+    limitNines = 0
+    textIn = convertToText(Math.round(Math.max(Math.min(numberIn, 10 ** digits - 1), 0)))
+    while (textIn.length < digits) {
+        textIn = " " + textIn
+    }
+    return textIn
+}
+function printSay () {
+    pushPrint(2, "R" + padLimit(Math.round(lastLaserR / 100), 1) + " C" + padLimit(Math.round(lastLaserC / 100), 1) + " L" + padLimit(Math.round(lastLaserL / 100), 1))
+}
+let lastHue = 0
+let lastGesture = 0
+let lastSonarRead = 0
+let lastVolumeRead = 0
+let textIn = ""
+let limitNines = 0
+let pushPrint2 = ""
+let lastLaserC = 0
+let lastLaserL = 0
+let lastLaserR = 0
 let scoreCircle: Connected.Strip = null
 let wheelLights: Connected.Strip = null
 let sockLights: Connected.Strip = null
 let digits: Connected.TM1637LEDs = null
+let isCastleSay = false
 pins.setAudioPinEnabled(false)
 led.enable(false)
-let isCastleSay = false
 if (notLegos.SonarFirstRead(DigitalPin.P8, DigitalPin.P9) > 0) {
     isCastleSay = true
 }
 Connected.oledClear()
 Connected.showUserText(1, "SAY: " + convertToText(isCastleSay))
-let lastLaserR = 0
-let lastHue = 0
-let lastGesture = 0
-let lastLaserC = 0
-let lastLaserL = 0
-let lastSonarRead = 0
-let lastVolumeRead = 0
 if (isCastleSay) {
     notLegos.potSet(AnalogPin.P10)
     digits = Connected.tm1637Create(
@@ -31,19 +55,28 @@ if (isCastleSay) {
     DigitalPin.P6
     )
     sockLights = Connected.create(Connected.DigitalRJPin.P11, 8, Connected.NeoPixelMode.RGB)
-    sockLights.showColor(Connected.colors(Connected.NeoPixelColors.Blue))
+    sockLights.showRainbow(1, 360)
     sockLights.show()
     wheelLights = Connected.create(Connected.DigitalRJPin.P12, 18, Connected.NeoPixelMode.RGB)
-    wheelLights.showColor(Connected.colors(Connected.NeoPixelColors.Blue))
+    wheelLights.showRainbow(1, 360)
     wheelLights.show()
     scoreCircle = Connected.create(Connected.DigitalRJPin.P13, 8, Connected.NeoPixelMode.RGB)
-    scoreCircle.showColor(Connected.colors(Connected.NeoPixelColors.Blue))
+    scoreCircle.showRainbow(1, 360)
     scoreCircle.show()
-    notLegos.sendMP3fileFolder("001", "005", SerialPin.P15)
+    lastLaserR = 0
+    lastLaserL = 0
+    lastLaserC = 0
 } else {
 	
 }
 let isReady = true
+for (let index = 0; index < 7; index++) {
+    let printLines: string[] = []
+    printLines.push("")
+}
+pushPrint2 = ""
+notLegos.sendMP3fileFolder("1", "1", SerialPin.P15)
+basic.pause(5000)
 control.inBackground(function () {
     while (isReady) {
         if (isCastleSay) {
@@ -52,16 +85,18 @@ control.inBackground(function () {
             lastLaserR = pins.analogReadPin(AnalogReadWritePin.P1)
             lastLaserC = pins.analogReadPin(AnalogReadWritePin.P2)
             lastLaserL = pins.analogReadPin(AnalogReadWritePin.P3)
-            lastHue = Connected.readColor()
             lastGesture = Connected.getGesture()
+            lastHue = Connected.readColor()
+            sockLights.rotate(1)
+            wheelLights.rotate(1)
+            scoreCircle.rotate(1)
+            sockLights.show()
+            wheelLights.show()
+            scoreCircle.show()
+            printSay()
         } else {
         	
         }
         basic.pause(40)
     }
-})
-loops.everyInterval(200, function () {
-    Connected.showUserText(2, "R" + lastLaserR + " C" + lastLaserC + " L" + lastLaserL)
-    Connected.showUserText(3, "dist" + lastSonarRead + " vol" + lastVolumeRead)
-    Connected.showUserText(4, "hue" + Math.round(lastHue) + " gest" + lastGesture)
 })
